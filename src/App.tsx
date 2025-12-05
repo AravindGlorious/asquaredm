3eccbeb8cafa43c0419164d816ffbc8fad9170ea
aravind <113155251+AravindGlorious@users.noreply.github.com>
2025-12-05 20:35:54 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import routes from './routes';
+
+const App: React.FC = () => {
+  return (
+    <HelmetProvider>
+      <Router>
+        <div className="flex flex-col min-h-screen">
+          <main className="flex-grow">
+            <Routes>
+              {routes.map((route, index) => (
+                <Route
+                  key={index}
+                  path={route.path}
+                  element={route.element}
+                />
+              ))}
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </HelmetProvider>
+  );
+};
+
+export default App;
